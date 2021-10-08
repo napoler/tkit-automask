ccc390c5aff58246cc6d8b702fee1124ea066aff
Terry chan <napoler2008@gmail.com>
2021-10-08 21:45:00 +0800

    Signed-off-by: Terry chan <napoler2008@gmail.com>

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-name='tkitDemo'#修改包名字-
-version='0.0.0.2 '
-description='Terry toolkit tkitDemo'
+name='tkitAutoMask'#修改包名字-
+version='0.0.0.1 '
+description='Terry toolkit tkitAutoMask'
 author='Terry Chan'
 author_email='napoler2008@gmail.com'
-url='http://www.terrychan.org/python_libs_demo/'
+url='https://terrychan.org/'
 
 copyright = '2021, Terry Chan'
 language = 'zh_cn'
