bb15cde34a37d13f4f8b81b7b93db05e658bd838
Terry chan <napoler2008@gmail.com>
2021-12-13 22:45:47 +0800

    Update mask.py

--- spanMaskDemo.py
+++ spanMaskDemo.py
@@ -11,9 +11,12 @@ from tkitAutoMask import BertRandomMaskingScheme
 from transformers import BertTokenizer, BertModel
 import torch
 tokenizer = BertTokenizer.from_pretrained('bert-base-chinese')
+MAX_LENGTH=512
 # model = BertModel.from_pretrained('bert-base-chinese')
 # inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
-inputs = tokenizer("Hello, my dog is cute")
+
+text="""与MarkusOdenthal类似，我的列表中有一个非字符串类型。我通过将列转换为字符串，然后将其转换为列表，然后将其拆分为训练段和测试段来修复它。你会这么做吗与MarkusOdenthal类似，我的列表中有一个非字符串类型。我通过将列转换为字符串，然后将其转换为列表，然后将其拆分为训练段和测试段来修复它。你会这么做吗"""
+inputs = tokenizer([text]*5, padding="max_length", max_length=MAX_LENGTH, truncation=True)
 # outputs = model(**inputs)
 # last_hidden_states = outputs.last_hidden_state
 
@@ -25,7 +28,7 @@ inputs = tokenizer("Hello, my dog is cute")
 print("inputs",inputs)
 class ARG:
   def __init__(self):
-    self.mask_ratio=0.4
+    self.mask_ratio=0.15
 args=ARG()
 args.mask_ratio
 
@@ -33,11 +36,33 @@ args.mask_ratio
 mk=BertRandomMaskingScheme(args, tokenizer.vocab_size, tokenizer.pad_token_id ,tokenizer.mask_token_id)
 print(dir(mk))
 
-
 out = mk.mask(inputs['input_ids'])
+print(out)
 # (array([  103,   100,   117,  5745, 13030,  8310, 12322,  8154,   102]), array([  101,     0,     0,  8422,     0,     0, 12322,     0,   102]), None)
 
-print(out)
+
+
+
+
+#
+# mk= PairWithSpanMaskingScheme(args, tokenizer.vocab_size, tokenizer.pad_token_id ,tokenizer.mask_token_id)
+# print(dir(mk))
+#
+# out = mk.mask(inputs['input_ids'])
+# print(out)
+# # (array([  103,   100,   117,  5745, 13030,  8310, 12322,  8154,   102]), array([  101,     0,     0,  8422,     0,     0, 12322,     0,   102]), None)
+#
+#
+
+
+
+
+
+
+
+
+
+
 
 
 
