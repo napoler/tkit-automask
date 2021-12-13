18feabd1aa874afd0809040562db7a5cbdd99f4d
Terry chan <napoler2008@gmail.com>
2021-12-13 22:57:23 +0800

    Update mask.py

--- spanMaskDemo.py
+++ spanMaskDemo.py
@@ -16,8 +16,11 @@ MAX_LENGTH=512
 # inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
 
 text="""与MarkusOdenthal类似，我的列表中有一个非字符串类型。我通过将列转换为字符串，然后将其转换为列表，然后将其拆分为训练段和测试段来修复它。你会这么做吗与MarkusOdenthal类似，我的列表中有一个非字符串类型。我通过将列转换为字符串，然后将其转换为列表，然后将其拆分为训练段和测试段来修复它。你会这么做吗"""
-inputs = tokenizer([text]*5, padding="max_length", max_length=MAX_LENGTH, truncation=True)
+# inputs = tokenizer([text]*5, padding="max_length", max_length=MAX_LENGTH, truncation=True)
+
+inputs = tokenizer([text]*5, padding="max_length", max_length=MAX_LENGTH, truncation=True, return_tensors="pt")
 # outputs = model(**inputs)
+
 # last_hidden_states = outputs.last_hidden_state
 
 # args, tokens, pad, mask_id=None,
@@ -34,13 +37,13 @@ args.mask_ratio
 
 # tokens=21128
 mk=BertRandomMaskingScheme(args, tokenizer.vocab_size, tokenizer.pad_token_id ,tokenizer.mask_token_id)
-print(dir(mk))
+# print(dir(mk))
 
 out = mk.mask(inputs['input_ids'])
 print(out)
 # (array([  103,   100,   117,  5745, 13030,  8310, 12322,  8154,   102]), array([  101,     0,     0,  8422,     0,     0, 12322,     0,   102]), None)
 
-
+print(torch.Tensor(out[0]),torch.Tensor(out[1]))
 
 
 
